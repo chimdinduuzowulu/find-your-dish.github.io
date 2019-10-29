9e0a29f0a796c0f454243ca1930838d789ca53f1
chimdindu uzowulu <chimdindu73@gmail.com>
2019-10-29 20:35:14 +0100

    just uploding the site to the internet

---
+++ contactus_page.js
@@ -0,0 +1,73 @@
+var hold=document.getElementById("send");
+hold.addEventListener("click",
+function submit()
+{
+    alert ("your details has been recived succesfully");
+}
+li
+);
+var dis=document.getElementById("form");
+dis.style.display="none";
+var fdis=document.getElementById("show");
+fdis.addEventListener("click",function()
+{
+    document.getElementById("form").style.display="block";
+}
+
+);
+// selecting the required input values for the form and setting the border to red
+// var check=true;
+// var send_data=document.getElementById("send");
+// var hold1=document.getElementById("in1");
+// holdd1=hold1.value;
+// var hold2=document.getElementById("in2");
+// holdd2=hold2.value;
+// var hold3=document.getElementById("in3");
+// holdd3=hold3.value;
+// var hold4=document.getElementById("in1");
+// holdd4=hold4.value;
+// send_data.addEventListener("click",function()
+// {
+//     if(holdd1=="" || holdd2=="" || holdd3=="" || holdd4=="")
+//     {
+//             hold1.style.border="2px solid red";
+//             hold2.style.border="2px solid red";
+//             hold3.style.border="2px solid red";
+//             hold4.style.border="2px solid red";
+//             alert("all fields are required.");
+//             check=false;
+//     }
+//     else{
+//             alert("your input has been recieved and will get back to you in no time");
+//             check=true;
+//         }
+// }
+// );
+// if(check==false)
+// {
+//     document.getElementById("form").style.display="block";
+//      hold1.style.border="2px solid red";
+//             hold2.style.border="2px solid red";
+//             hold3.style.border="2px solid red";
+//             hold4.style.border="2px solid red";
+// }
+// if (check==true) 
+// {
+//     alert("your input has been recieved and will get back to you in no time");
+//     document.getElementById("form").style.display="none";
+//      hold1.style.border="";
+//             hold2.style.border="";
+//             hold3.style.border="";
+//             hold4.style.border="";
+// }
+
+// after it displays the red bother then show the form a
+
+
+
+// var again=document.getElementsByClassName("in");
+// undefined
+// again.fname;
+// <input type=​"text" placeholder=​"name here" name=​"fname" class=​"in">​
+// again.fname.value
+// ""
